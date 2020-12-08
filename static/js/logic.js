64e74be5bc15f4749e9b0bb855de16330adb86a2
Dave Ruppel <drupps@gmail.com>
2020-12-07 16:37:38 -0800

    code additions

--- static/js/logic.js
+++ static/js/logic.js
@@ -2,7 +2,7 @@
 var queryURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
 // Perform a GET request to the query URL
-d3.json(queryUrl, function(data) {
+d3.json(queryURL, function(data) {
   console.log(data);
     // Once we get a response, send the data.features object to the createFeatures function
     for (var i = 0; i < response.length; i++) {
