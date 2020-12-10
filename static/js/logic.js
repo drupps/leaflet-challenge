6ad0fb7fa70f19a225271e99e2f801c9e27b91a0
Dave Ruppel <drupps@gmail.com>
2020-12-10 14:59:09 -0800

    satellite does not work

--- static/js/logic.js
+++ static/js/logic.js
@@ -52,26 +52,39 @@ function createMap(earthquakes) {
       tileSize: 512,
       maxZoom: 18,
       zoomOffset: -1,
-      id: "mapbox/streets-v11",
+      id: "mapbox/light-v10",
       accessToken: API_KEY,
     }
   );
 
-  var darkmap = L.tileLayer(
-    "https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+  var satellite = L.tileLayer(
+    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
     {
       attribution:
-        'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+      tileSize: 512,
       maxZoom: 18,
-      id: "dark-v10",
+      zoomOffset: -1,
+      id: "mapbox/satellite",
       accessToken: API_KEY,
     }
   );
+
+  // var darkmap = L.tileLayer(
+  //   "https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+  //   {
+  //     attribution:
+  //       'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  //     maxZoom: 18,
+  //     id: "dark-v10",
+  //     accessToken: API_KEY,
+  //   }
+  // );
 
   // Define a baseMaps object to hold our base layers
   var baseMaps = {
     "Street Map": streetmap,
-    "Dark Map": darkmap,
+    "Satellite": satellite
   };
 
   // Create overlay object to hold our overlay layer
@@ -94,4 +107,33 @@ function createMap(earthquakes) {
       collapsed: false,
     })
     .addTo(myMap);
+}
+
+
+// Loop through the cities array and create one marker for each city object
+for (var i = 0; i < earthquakeData.length; i++) {
+
+  // Conditionals for countries points
+  var color = "";
+  if (countries[i].points > 90) {
+    color = "red";
+  }
+  else if (countries[i].points > 100) {
+    color = "blue";
+  }
+  else if (countries[i].points > 90) {
+    color = "green";
+  }
+  else {
+    color = "red";
+  }
+
+  // Add circles to map
+  L.circle(countries[i].location, {
+    fillOpacity: 0.75,
+    color: "white",
+    fillColor: color,
+    // Adjust radius
+    radius: countries[i].points * 1500
+  }).bindPopup("<h1>" + countries[i].name + "</h1> <hr> <h3>Points: " + countries[i].points + "</h3>").addTo(myMap);
 }
